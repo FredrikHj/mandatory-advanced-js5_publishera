f64a2a280c5429cd3cc0f681ec092171d974eb6f
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2019-05-20 19:49:58 +0200

    Updates

---
+++ precache-manifest.caf456ce7363f6c8dfa878306b23c0f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe5d61f53655a4dc364c6f8ddd1775b",
+    "url": "/mandatory-advanced-js5_publishera/index.html"
+  },
+  {
+    "revision": "90b4503db7711fdea1f9",
+    "url": "/mandatory-advanced-js5_publishera/static/css/main.888cd3b7.chunk.css"
+  },
+  {
+    "revision": "bb8cd8edd8cc27caec91",
+    "url": "/mandatory-advanced-js5_publishera/static/js/2.389bdf51.chunk.js"
+  },
+  {
+    "revision": "90b4503db7711fdea1f9",
+    "url": "/mandatory-advanced-js5_publishera/static/js/main.f06aca86.chunk.js"
+  },
+  {
+    "revision": "445630f9e4fe217951d6",
+    "url": "/mandatory-advanced-js5_publishera/static/js/runtime~main.fddf9744.js"
+  }
+]);
